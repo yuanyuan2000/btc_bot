af4c69a0051324c7c16b9589e239d07c253e29eb
Gerry Zhang <22994257@student.uwa.edu.au>
2023-04-28 00:01:18 +0800

    basic app

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# btc_bot
+# README.md
+
+## To start development:
+1.  Clone this repository to your computer
+2.  Open the cloned folder in VS Code
+3.  Open Terminal in VS Code (Terminal > New Terminal)
+4.  Switch to the develop branch in Terminal `git checkout develop` (Or switch the branch in the bottom left corner of VS Code)
+5.  Create a new conda environment and activate it in your local machine by `conda env create -f env.yml` and `conda activate btcbot`
+6.  Setting the VS Code to use the conda environment you just created (**Ctrl + Shift + P > Python: Select Interpreter > btcbot**)
+7.  Run the code in **main.ipynb**
+
+   
+## Update some packages during development:
+1.  If you want to install some packages, please remember to update the **env.yml** after activating the **btcbot**. 
+2.  Export the dependencies of the current environment to the **env.yml** by `conda env export --no-builds > env.yml`
+3.  If you want to deactive the conda environment, please use `conda deactivate`
+4.  If you want to delete your local conda environment, please use `conda env remove -n btcbot`
+
+## Git commands:
+1.  If you want to push your code, please use `git push origin <branch-name>` after using `git add .` and `git commit -m "<your commit message>"` (You need to set the `origin` url first)
+2.  If you want to pull the latest code, please use `git pull origin <branch-name>` (You need to set the `origin` url first)
+3.  If you want to create a new branch, please use `git checkout -b <branch-name>` (We use the develop branch as the default development branch, and when we finish a version, we will merge the develop branch to the main branch)
--- get_data.py
+++ get_data.py
@@ -2,8 +2,8 @@ import ccxt
 import pandas as pd
 from ta import add_all_ta_features
 from ta.trend import SMAIndicator, EMAIndicator
-import IPython
 
+# fetch the data from kraken and return it as a dataframe
 def fetch_ohlcv_data(exchange, symbol, timeframe, limit):
     data = exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
     header = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
@@ -11,22 +11,37 @@ def fetch_ohlcv_data(exchange, symbol, timeframe, limit):
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
     return df
 
-def add_sma_and_ema(data, column_name, window):
+# get the SMA for the close price with a "window" days window
+def add_sma(data, column_name, window):
     sma_indicator = SMAIndicator(data[column_name], window=window)
-    ema_indicator = EMAIndicator(data[column_name], window=window)
     data[f'sma_{window}'] = sma_indicator.sma_indicator()
-    data[f'ema_{window}'] = ema_indicator.ema_indicator()
+    return data
 
-def get_data():
+# get the EMA for the close price with a "window" days window
+def add_ema(data, column_name, window):
+    ema_indicator = EMAIndicator(data[column_name], window=window)
+    data[f'ema_{window}'] = ema_indicator.ema_indicator()
+    return data
+
+# main function, returns the dataframe with all the data and technical indicators
+def grab_data():
     exchange = ccxt.kraken()
     symbol = 'BTC/AUD'
     timeframe = '1d'  # 1 day candles
     limit = 720
 
+    # Fetch the data
     data = fetch_ohlcv_data(exchange, symbol, timeframe, limit)
-    data_with_ta = add_all_ta_features(data, open="open", high="high", low="low", close="close", volume="volume")
     
-    # Add SMA and EMA for the close price with a 20-days window
-    add_sma_and_ema(data_with_ta, 'close', 20)
+    # Add SMA and EMA for the close price with a 9-days/21-days window
+    data = add_sma(data, 'close', 9)
+    data = add_sma(data, 'close', 21)
+    data = add_ema(data, 'close', 9)
+    data = add_ema(data, 'close', 21)
+
+    # Add all ta features
+    data = add_all_ta_features(data, open="open", high="high", low="low", close="close", volume="volume")
+
+    return data
     
-    #test comment
+
