cd9d8d58a2c6f9cac8f563232ac82d612d28339b
Gerry Zhang <22994257@student.uwa.edu.au>
2023-05-02 21:34:14 +0800

    fix some bug and show the plot

--- Asset.py
+++ Asset.py
@@ -8,35 +8,11 @@ class Asset:
         self.tax = 0.02
         self.coin = 0
 
-    def buy(self):
-        money_in_market = self.money * (1 - self.tax)  # add conversion AUD -> bitcoin
+    def buy(self, coin_value):
+        money_in_market = self.money * (1 - self.tax)
         self.money = 0
-        return money_in_market
+        self.coin = money_in_market / coin_value
 
-    def sell(self, money_in_market):
-        self.money = money_in_market * (1 - self.tax)  # add conversion bitcoin -> AUD
-
-    def trade(self, data: pd.DataFrame, windowsize, highthreshold, lowthreshold):
-        in_position = False
-        rsi = RSIIndicator(data["close"], window=windowsize)
-        data["rsi"] = rsi.rsi()
-
-        for i in range(len(data) - 1):
-            if not in_position and data["rsi"].iloc[i] < lowthreshold:
-                # Buy
-                self.coin = self.buy() / data['close'].iloc[i + 1]
-                in_position = True
-                print(f'Buy at the {i} day')
-            elif in_position and data["rsi"].iloc[i] > highthreshold:
-                # Sell
-                self.sell(self.coin * data['close'].iloc[i + 1])
-                self.coin = 0
-                in_position = False
-                print(f'Sell at the {i} day. Now we have {self.money} AUD.')
-
-        # Final sell at the end of the period
-        if in_position:
-            self.sell(self.coin * data['close'].iloc[-1])
-            self.coin = 0
-
-        return self.money
+    def sell(self, coin_value):
+        self.money = self.coin * coin_value * (1 - self.tax)
+        self.coin = 0
