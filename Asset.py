fdfda41075b7828e73f46fc63fd10577defb4ff7
Gerry Zhang <22994257@student.uwa.edu.au>
2023-05-02 21:54:01 +0800

    update

--- Asset.py
+++ Asset.py
@@ -7,13 +7,11 @@ class Asset:
         self.tax = 0.02
         self.coin = 0
 
-    def buy(self):
-        money_in_market = self.money * (1 - self.tax)  # add conversion AUD -> bitcoin
+    def buy(self, coin_value):
+        money_in_market = self.money * (1 - self.tax)
         self.money = 0
         self.coin = money_in_market / coin_value
 
-    def sell(self, money_in_market):
-        self.money = money_in_market * (1 - self.tax)  # add conversion bitcoin -> AUD
+    def sell(self, coin_value):
+        self.money = self.coin * coin_value * (1 - self.tax)
         self.coin = 0
-
-
